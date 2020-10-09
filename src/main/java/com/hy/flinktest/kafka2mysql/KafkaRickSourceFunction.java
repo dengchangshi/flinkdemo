1631f8cf31245470e3f0bd591a8a6d49eafbcd80
dengchangshi <dengcs1@hua-cloud.com.cn>
2020-10-09 17:16:33 +0800

    增加偏移量存储到zk,并从zk拉取偏移量进行消费

--- src/main/java/com/hy/flinktest/kafka2mysql/KafkaRickSourceFunction.java
+++ src/main/java/com/hy/flinktest/kafka2mysql/KafkaRickSourceFunction.java
@@ -2,26 +2,24 @@ package com.hy.flinktest.kafka2mysql;
 
 
 import com.hy.flinktest.entity.User;
+import com.hy.flinktest.service.ZookeeperService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.runtime.state.FunctionInitializationContext;
 import org.apache.flink.runtime.state.FunctionSnapshotContext;
 import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * ClassName: KafkaRickSourceFunction
@@ -35,6 +33,16 @@ public class KafkaRickSourceFunction extends RichSourceFunction<String> implemen
     //kafka
     private static Properties prop = new Properties();
     private boolean running = true;
+    private static ZookeeperService zkService;
+    static {
+        try {
+            zkService = new ZookeeperService();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     /*
@@ -50,34 +58,80 @@ public class KafkaRickSourceFunction extends RichSourceFunction<String> implemen
         prop.put("group.id",WritedatatoKafka.TOPIC_USER);
         prop.put("key.deserializer",WritedatatoKafka.CONST_DESERIALIZER);
         prop.put("value.deserializer",WritedatatoKafka.CONST_DESERIALIZER);
-        prop.put("auto.offset.reset","latest");//earliest
-        prop.put("max.poll.records", "500");
-        prop.put("auto.commit.interval.ms", "1000");
+        prop.put("auto.offset.reset","earliest");//earliest，latest
+        prop.put("max.poll.records", "100");//每次拉取100条数据
+        //prop.put("auto.commit.interval.ms", "1000"); 每秒自动提交一次
         //换成下面这种写法，比较容易看
-        //prop.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false");
+        prop.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false");//关闭自动提交
     }
 
     @Override
-    public void run(SourceContext sourceContext) throws Exception {
+    public void run(SourceContext sourceContext) {
+        int count = 0;
         //创建一个消费者客户端实例
         KafkaConsumer<String,String> kafkaConsumer = new KafkaConsumer<String, String>(prop);
         //只消费TOPIC_USER 分区
         TopicPartition topicPartition = new TopicPartition(WritedatatoKafka.TOPIC_USER,partition);
-        long offset =0; //这个初始值应该从zk或其他地方获取
+
+        long offset = 0;
+        String zkTopicPath = zkService.etlAddTaskPath+ "/" + topicPartition.topic() + "/" + topicPartition.partition();
+        try {
+            zkService.createOffsetNodeIfNotExist(topicPartition.topic(), topicPartition.partition());
+            offset = zkService.getOffset(zkTopicPath);//获取偏移量
+            log.info("从偏移量:{} 开始消费",offset);
+            System.out.println(String.format("从偏移量 %d 开始消费",offset));
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         offset = placeOffsetToBestPosition(kafkaConsumer, offset, topicPartition);
 
 
-        while (running){
-            ConsumerRecords<String, String> records = kafkaConsumer.poll(1000);
-            if(records.isEmpty()){
-                continue;
+       // final Map<TopicPartition,OffsetAndMetadata> currentOffset = new ConcurrentHashMap<>();
+
+        try {
+            while (running) {
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(1000);
+                if (records.isEmpty()) {
+                    continue;
+                }
+                for (ConsumerRecord<String, String> record : records) {
+                    //record.offset();
+                    //record.key()
+                  //  currentOffset.put(new TopicPartition(record.topic(),record.partition()),new OffsetAndMetadata(record.offset(),"metadata"));
+                    String value = record.value();
+                    sourceContext.collect(value);
+                    if (count % 5 == 0) {//每5条提交一次，如果要求实时性很高，可以去掉改判断，进行实时提交
+                        kafkaConsumer.commitAsync(new OffsetCommitCallback() {
+                            @Override
+                            public void onComplete(Map<TopicPartition, OffsetAndMetadata> map, Exception e) {
+                                if(e != null){
+                                    log.error("commit failed"+map);//这里异常回调，跟下面catch异常捕获处理机制一样，选择一种就可以了
+                                    //System.out.println("commit failed"+map);
+                                    //todo something,错误补偿机制
+                                }
+
+                            }
+                        });
+                    }
+                    log.info("存储偏移量:{}",record.offset()+1);
+                    System.out.println("存储偏移量:"+String.valueOf(record.offset()+1));
+                    zkService.saveOffset(zkTopicPath,record.offset()+1);
+                    count++;
+                }
             }
-            for (ConsumerRecord<String, String> record : records) {
-                //record.offset();
-                //record.key()
-                String value = record.value();
-                sourceContext.collect(value);
+        }catch (Exception e){
+            log.error("提交失败，进行异常处理");
+            //kafkaConsumer.commitAsync(currentOffset,null);
+            //todo something,错误补偿机制
+        }finally {
+            try {
+                kafkaConsumer.commitSync();//最后进行一次同步提交
+            }finally {
+                kafkaConsumer.close();
             }
+
         }
 
     }
